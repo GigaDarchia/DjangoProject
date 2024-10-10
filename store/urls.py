d6c11fe98298fbe049c759440f3db0fb1f12c216
Giga <sebagedarchia@gmail.com>
2024-10-10 12:16:46 +0400

    Add new product and category URL patterns

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import store, product
+from .views import store, product, categories, products
 
 urlpatterns = [
     path('', store, name='store'),
     path('product/', product, name='product'),
+    path('categories/', categories, name='categories'),
+    path('products/', products, name='products')
 ]
