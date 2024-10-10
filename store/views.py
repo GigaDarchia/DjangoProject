22f19bbe1db11bb19d3acbd8b6bd069e4d956446
Giga <sebagedarchia@gmail.com>
2024-10-10 12:15:46 +0400

    Add category and product JSON responses, update HTML to add new buttons

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Product, Category
 def store(request):
     html = """
     <!DOCTYPE html>
@@ -35,7 +36,7 @@ def store(request):
         nav a:clicked {
             color: #2be715;
         }
-        #cart {
+        .cart {
             background-color: #23bf11;
             color: white;
             transition-duration: .5s;
@@ -43,11 +44,12 @@ def store(request):
             padding: 10px;
             border-radius: 5px;
         }
-        #cart:hover {
+        .cart:hover {
             background-color: #2be715;
         }
         main a {
             text-decoration: none;
+            color: black;
         }
         </style>
         <title>Store</title>
@@ -67,7 +69,9 @@ def store(request):
             <p>This is the store page.</p>
             <div class="products">
                 <p>Product 1</p>
-                <button id="cart"><a href="/store/product">Add to cart</a></button>
+                <button class="cart"><a href="/store/product">Add to cart</a></button>
+                <button class="cart"><a href="/store/categories">Show categories</a></button>
+                <button class="cart"><a href="/store/products">Show products</a></button>
             </div>
         </main>
       </body>
@@ -76,3 +80,41 @@ def store(request):
     return HttpResponse(html)
 def product(request):
     return HttpResponse("Added to cart successfully.")
+
+def categories(request):
+    category_list = []
+
+    for category in Category.objects.all():
+        category_list.append({
+            'id': category.id,
+            "name": category.name,
+            "img": category.img.name if category.img else None,
+            "parent": {
+                "id": category.parent.id,
+                "name": category.parent.name
+              } if category.parent else None
+        })
+
+    return JsonResponse(category_list, safe=False)
+
+def products(request):
+    product_list = []
+
+    for product in Product.objects.all():
+        category_list = []
+        for category in product.category.all():
+            category_list.append({
+                'id': category.id,
+                'name': category.name
+            })
+
+        product_list.append({
+            'id': product.id,
+            'name': product.title,
+            'price': product.price,
+            'image': product.img.name if product.img else None,
+            "description": product.desc if product.desc else None,
+            'categories': category_list if product.category.count() > 0 else None
+        })
+
+    return JsonResponse(product_list, safe=False)
